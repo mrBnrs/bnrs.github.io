13948923e66e04718a2135aafc7473ca5ada780a
Andy.Z <wizzout@icloud.com>
2018-10-01 11:09:52 +0600

    secondmap

---
+++ second-map/precache-manifest.4c3ef8733896d56f83d4dbc7cc721d36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a9c4414554b26ad7de46",
+    "url": "/static/js/main.a9c44145.chunk.js"
+  },
+  {
+    "revision": "40e3cf491402cc6f151a",
+    "url": "/static/js/1.40e3cf49.chunk.js"
+  },
+  {
+    "revision": "a9c4414554b26ad7de46",
+    "url": "/static/css/main.ee879f24.chunk.css"
+  },
+  {
+    "revision": "40e3cf491402cc6f151a",
+    "url": "/static/css/1.12db6123.chunk.css"
+  },
+  {
+    "revision": "d6e87134b46c0e7426e74f9b903c1f2e",
+    "url": "/index.html"
+  }
+];
